5beb90ce9ab76aeb3eb6925533b7d7f281e1ef6e
Alex <nohacvvi@gmail.com>
2022-07-08 13:39:42 +0300

    project creation, elevator movement functionality

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for="i in floors" :key="i" class="floor">
+    <div class="floor__cell">
+      <div
+        v-if="floor === i"
+        class="elevator"
+        :style="[moving && {'transform': `translate(0, ${-100 * (floor-i)}px`}]"
+      >
+        <div class="elevator__inner"></div>
+      </div>
+    </div>
+    <div class="floor__btn">
+      <span>{{ i }}</span>
+      <button @click="moveElevator(i)" class="btn">0</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data () {
+    return {
+      floors: [6, 5, 4, 3, 2, 1],
+      floor: 1,
+      moving: false
+    }
+  },
+  methods: {
+    moveElevator (floor) {
+      this.floor = floor;
+      this.moving = true
+      console.log(this.floor);
+      // while(this.floor !== floor) {
+
+      // }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import './styles/global.scss';
+
+.floor {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  padding-left: 15px;
+  border-top: 1px solid var(--color-grey);
+  &:last-child {
+    border-bottom: 1px solid var(--color-grey);
+  }
+  &__cell {
+    width: 100px;
+    height: 100%;
+    border-right: 3px solid var(--color-grey);
+    border-left: 3px solid var(--color-grey);
+  }
+  &__btn {
+    margin-left: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+.elevator {
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-blue);
+  transition: all 1s linear;
+  &-move {
+    transform: translate(0, -100px);
+  }
 }
 </style>
